8ced9e32de5f395809a55395d63b333e5ed271e5
Le Van Vuong <vuonglevan@vccorp.vn>
2020-01-30 15:36:36 +0700

    install docker script 2020

---
+++ docker-install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#uninstall old versions
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sudo apt-get update
+
+#Install packages to allow apt to use a repository over HTTPS
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+#add docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+# setup the stable repository
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+#install lastest version of docker engine and containerd
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+#pull hello-world for test
+sudo docker run hello-world
